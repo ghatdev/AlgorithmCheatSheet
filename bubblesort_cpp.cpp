bb5b233f26c50337e5709c83103973f72ecc3bfa
SeongUk Cho <issacs98@gmail.com>
2017-05-18 20:23:42 +0900

    Update and rename quicksort_cpp.cpp to bubblesort_cpp.cpp

--- bubblesort_cpp.cpp
+++ bubblesort_cpp.cpp
@@ -1,5 +1,5 @@
 /**
- quick sorting 알고리즘
+ bubblesort sorting 알고리즘
  main함수 위에 붙여넣어서 쓰면 됩니다.
  정렬하고픈 int형 배열 주소를 인수로 주면 됩니다.
  이건 오름차순이에요. 내림차순으로 하려면 i<(len_arr-1) 부분의 부등호 >로 바꿔주면 됩니다.
@@ -7,12 +7,12 @@
  int main(void)
  {
  	int test[] = {1,4,2,3,5};
-	quicksort(test);
+	bubblesort(test);
 	return 0;
  }
 */
 
-void quicksort(int * arr){
+void bubblesort(int * arr){
 	int len_arr = sizeof(arr);
 	for (int j=0; j<len_arr;j++){
 		for (int i=0;i<(len_arr-1);i++){
